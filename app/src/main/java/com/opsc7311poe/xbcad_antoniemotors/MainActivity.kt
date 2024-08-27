7a32d8bda565456bd7de1b5fb58261320da9198f
ST10022447 Perla Jbara <st10022447@vcconnect.edu.za>
2024-08-27 21:07:04 +0200

    The following changes were made: Fragments Quote, receipt, home, services and employees were added. The bottom navigation bar has been implemented and functions. Added a new resource file to create a red to green gradient around textboxes. Added Splashscreen activity and Login screen activity. Began login screen design. Added Login Car image file.

--- app/src/main/java/com/opsc7311poe/xbcad_antoniemotors/MainActivity.kt
+++ app/src/main/java/com/opsc7311poe/xbcad_antoniemotors/MainActivity.kt
@@ -1,20 +1,62 @@
 package com.opsc7311poe.xbcad_antoniemotors
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var bottomNavView: BottomNavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        bottomNavView = findViewById(R.id.bottom_navigation)
+
+        replaceFragment(HomeFragment())
+        bottomNavView.setOnItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.navHome -> {
+                    replaceFragment(HomeFragment())
+                    true
+                }
+                R.id.navReceipt -> {
+                    replaceFragment(ReceiptFragment())
+                    true
+                }
+                R.id.navEmployees -> {
+                    replaceFragment(EmployeeFragment())
+                    true
+                }
+                R.id.navServices -> {
+                    replaceFragment(ServicesFragment())
+                    true
+                }
+                R.id.navQuoteGen -> {
+                    replaceFragment(QuoteFragment())
+                    true
+                }
+                else -> false
+            }
+        }
+
+
+        if (savedInstanceState == null) {
+            replaceFragment(HomeFragment())
+
         }
     }
+
+    private fun replaceFragment(fragment: Fragment) {
+        Log.d("MainActivity", "Replacing fragment: ${fragment::class.java.simpleName}")
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.frame_container, fragment)
+            .commit()
+    }
 }
+
