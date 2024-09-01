5fe67961698165000f9d4cdff252a153dd6008af
GabbiJ <gabriellajanssen9@gmail.com>
2024-09-01 14:45:40 +0200

    Finished fragment_services.xml

--- app/src/main/java/com/opsc7311poe/xbcad_antoniemotors/ServicesFragment.kt
+++ app/src/main/java/com/opsc7311poe/xbcad_antoniemotors/ServicesFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -21,12 +23,17 @@ class ServicesFragment : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
 
+    private lateinit var btnPlus: Button
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
+
     }
 
     override fun onCreateView(
