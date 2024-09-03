1c55a1fb2c5663d10f2b35d8bd8271abb1cbc47d
ST10022447 Perla Jbara <st10022447@vcconnect.edu.za>
2024-09-03 11:04:57 +0200

    minor changes to home page. Added further navigation.

--- app/src/main/java/com/opsc7311poe/xbcad_antoniemotors/Login.kt
+++ app/src/main/java/com/opsc7311poe/xbcad_antoniemotors/Login.kt
@@ -1,15 +1,29 @@
 package com.opsc7311poe.xbcad_antoniemotors
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
 
 class Login : AppCompatActivity() {
+
+    private lateinit var btnLogin: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        btnLogin = findViewById(R.id.btnLogin)
+
+        btnLogin.setOnClickListener(){
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }
     }
+
+
 }
