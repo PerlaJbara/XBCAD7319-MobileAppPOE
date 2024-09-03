fed24c0482fe0e2b719967104096ea7788fc5981
GabbiJ <gabriellajanssen9@gmail.com>
2024-09-03 21:37:58 +0200

    Added all of my fragments, busy doing add service fragment. Got plus button working on ServicesFragment.

--- app/src/main/java/com/opsc7311poe/xbcad_antoniemotors/ServicesFragment.kt
+++ app/src/main/java/com/opsc7311poe/xbcad_antoniemotors/ServicesFragment.kt
@@ -1,66 +1,40 @@
 package com.opsc7311poe.xbcad_antoniemotors
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageButton
+import android.widget.ImageView
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [ServicesFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ServicesFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    private lateinit var btnPlus: Button
+    private lateinit var imgPlus: ImageView
 
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_services, container, false)
+        val view = inflater.inflate(R.layout.fragment_services, container, false)
+
+        imgPlus = view.findViewById(R.id.imgPlus)
+
+        imgPlus.setOnClickListener(){
+            replaceFragment(AddServiceFragment())
+        }
+
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ServicesFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ServicesFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    private fun replaceFragment(fragment: Fragment) {
+        Log.d("ServicesFragment", "Replacing fragment: ${fragment::class.java.simpleName}")
+        parentFragmentManager.beginTransaction()
+            .replace(R.id.frame_container, fragment)
+            .commit()
     }
 }
