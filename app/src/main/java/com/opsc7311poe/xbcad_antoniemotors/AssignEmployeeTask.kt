d4222e4cf7c65be0124add10cae756064243208d
ST10022447 Perla Jbara <st10022447@vcconnect.edu.za>
2024-10-17 18:54:29 +0200

    Assign Employee Task Completed

---
+++ app/src/main/java/com/opsc7311poe/xbcad_antoniemotors/AssignEmployeeTask.kt
@@ -0,0 +1,31 @@
+package com.opsc7311poe.xbcad_antoniemotors
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class AssignEmployeeTask : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_assign_employee_task, container, false)
+
+
+    }
+
+
+
+}
