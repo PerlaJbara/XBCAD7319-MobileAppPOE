47c54b59b23644a252084b602e0b761c364b979b
Daniel-Antonie <104687456+Daniel-Antonie@users.noreply.github.com>
2024-11-11 20:54:21 +0200

    Changed Vehicle menu fragment design

--- app/src/main/java/com/opsc7311poe/xbcad_antoniemotors/VehicleMenuFragment.kt
+++ app/src/main/java/com/opsc7311poe/xbcad_antoniemotors/VehicleMenuFragment.kt
@@ -6,12 +6,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
 
 class VehicleMenuFragment : Fragment() {
 
-    private lateinit var btnVehicleReg: Button
-    private lateinit var btnAddVehicleMakes: Button
+    private lateinit var btnVehicleReg: ImageView
+    private lateinit var btnAddVehicleMakes: ImageView
     private lateinit var btnPartsInventory: Button
 
     override fun onCreateView(
